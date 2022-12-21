96c90d4df85929100d104b3fc26119563d4888c2
Saakshi Saxena <saakshi.saxena2000@gmail.com>
2022-12-21 18:20:49 +0000

    Data extractor for web pages - working - with extra files - check page redder example changes made

---
+++ page-redder/background.js
@@ -0,0 +1,23 @@
+function reddenPage() {
+  document.body.style.backgroundColor = 'red';
+  console.log("Turns the page red");
+  // console.log("doc.body = "+document.body.innerHTML);
+  content = document.body.innerHTML;
+  content = content.replace( /(<([^>]+)>)/ig, '');
+  // console.log(content);
+  str = content;
+  // str=str.replaceAll("(?i)(^([a-z])\\.|(?<= )([a-z])\\.|(?<=\\.)([a-z])\\.)", "$2$3$4").trim();  
+  // str=str.replaceAll("(?i)^(([a-z]) ([a-z]))($| )", "$2$3 ").trim();
+  // str=str.replaceAll("(?i)(?<= )(([a-z]) ([a-z]))($| )", "$2$3 ").trim();
+  str = str.replace(/\s\s+/g, '. ');
+  console.log(str);
+}
+
+chrome.action.onClicked.addListener((tab) => {
+  if(!tab.url.includes("chrome://")) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: reddenPage
+    });
+  }
+});
