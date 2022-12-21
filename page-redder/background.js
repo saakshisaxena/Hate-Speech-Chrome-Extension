0c9fe844f25a8971760afc49a67eaa0c0229138d
Saakshi Saxena <saakshi.saxena2000@gmail.com>
2022-12-21 18:59:10 +0000

    Remove unwanted comments

--- page-redder/background.js
+++ page-redder/background.js
@@ -1,17 +1,11 @@
 function reddenPage() {
   document.body.style.backgroundColor = 'red';
   console.log("Turns the page red");
-  // console.log("doc.body = "+document.body.innerHTML);
   content = document.body.innerHTML;
   content = content.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '')
   content = content.replace(/<style\b[^<]*(?:(?!<\/style>)<[^<]*)*<\/style>/gi, '')
-  // console.log(content);
   content = content.replace( /(<([^>]+)>)/ig, '');
-  // console.log(content);
   str = content;
-  // str=str.replaceAll("(?i)(^([a-z])\\.|(?<= )([a-z])\\.|(?<=\\.)([a-z])\\.)", "$2$3$4").trim();  
-  // str=str.replaceAll("(?i)^(([a-z]) ([a-z]))($| )", "$2$3 ").trim();
-  // str=str.replaceAll("(?i)(?<= )(([a-z]) ([a-z]))($| )", "$2$3 ").trim();
   str = str.replace(/\s\s+/g, '. ');
   console.log(str);
 }
