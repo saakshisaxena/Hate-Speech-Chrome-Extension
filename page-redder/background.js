e9c6cf065be48bac64fa6eaeb11bdf85210c0542
Saakshi Saxena <saakshi.saxena2000@gmail.com>
2023-01-03 20:52:48 +0000

    Working go back button

--- page-redder/background.js
+++ page-redder/background.js
@@ -8,10 +8,24 @@ function reddenPage() {
   str = content;
   str = str.replace(/\s\s+/g, '. ');
   console.log(str);
+
+  // create the go back button
+  var x = document.createElement("BUTTON");
+  var t = document.createTextNode("GO BACK");
+  x.setAttribute("id","go-back-button");
+  x.appendChild(t);
+  // document.body.appendChild(x);
+
+  // add the blur screen
   const elem = document.createElement('div');
   elem.style.cssText = 'position: fixed; padding:0; top:0; left:0; width: 100%; height: 100%; background:rgba(24,24,24,0.95);';
+  elem.appendChild(x);
+  elem.setAttribute("id","blur-screen");
   document.body.appendChild(elem);
+
+  document.getElementById("go-back-button").onclick = function() {window.history.back(); elem.remove();};
 }
+
 
 chrome.action.onClicked.addListener((tab) => {
   if(!tab.url.includes("chrome://")) {
