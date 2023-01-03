3b1bddfe253bae5f7c27720e9086c7c33bbbb574
Saakshi Saxena <saakshi.saxena2000@gmail.com>
2023-01-03 21:22:56 +0000

    Feedback form skeleton done but not fully working

--- page-redder/background.js
+++ page-redder/background.js
@@ -16,24 +16,36 @@ function reddenPage() {
   x.appendChild(t);
   // document.body.appendChild(x);
 
-    // create the proceed button
-    var y = document.createElement("BUTTON");
-    var s = document.createTextNode("Proceed");
-    y.setAttribute("id","proceed-button");
-    y.appendChild(s);
+  // create the proceed button
+  var y = document.createElement("BUTTON");
+  var s = document.createTextNode("PROCEED");
+  y.setAttribute("id","proceed-button");
+  y.appendChild(s);
+
+  // Add a button for feedback
+  var z = document.createElement("BUTTON");
+  var q = document.createTextNode("FEEDBACK");
+  z.setAttribute("id","feedback-button");
+  z.appendChild(q);
 
   // add the blur screen
   const elem = document.createElement('div');
   elem.style.cssText = 'position: fixed; padding:0; top:0; left:0; width: 100%; height: 100%; background:rgba(24,24,24,0.95);';
   elem.appendChild(x);
   elem.appendChild(y);
+  elem.appendChild(z);
   elem.setAttribute("id","blur-screen");
   document.body.appendChild(elem);
 
+  document.getElementById("go-back-button").style = "margin: 10%;top: 75%;-ms-transform: translateY(-50%);transform: translateY(-50%);";
+  document.getElementById("proceed-button").style = "margin: 30%;top: 75%;-ms-transform: translateY(-50%);transform: translateY(-50%);";
+  document.getElementById("feedback-button").style = "margin: 0%;top: 75%;-ms-transform: translateY(-50%);transform: translateY(-50%);";
+
   document.getElementById("go-back-button").onclick = function() {window.history.back(); elem.remove();};
   document.getElementById("proceed-button").onclick = function() {elem.remove();};
-}
+  document.getElementById("feedback-button").onclick = function() { var x = document.createElement("FORM"); x.setAttribute("id", "myForm"); document.body.appendChild(x); var y = document.createElement("INPUT");y.setAttribute("type", "text");y.setAttribute("value", "Donald"); document.getElementById("myForm").appendChild(y);};
 
+}
 
 chrome.action.onClicked.addListener((tab) => {
   if(!tab.url.includes("chrome://")) {
