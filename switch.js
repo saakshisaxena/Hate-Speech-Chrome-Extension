fbe3827f82333888d50a03e1e819ba3fc3d11384
Saakshi Saxena <saakshi.saxena2000@gmail.com>
2023-01-29 14:54:34 +0000

    Remove extra files and rename project folder

--- switch.js
+++
@@ -1,4 +0,0 @@
-for (var i = 0; document.images.length; i++) {
-  // Change the url to anyting you want!
-  document.images[i].src = "https://images.pexels.com/photos/2775196/pexels-photo-2775196.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-};
